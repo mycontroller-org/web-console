10a4fd35de674beeb7591e5ca4f25024c08e1be8
Jeeva Kandasamy <jkandasa@gmail.com>
2021-04-14 12:43:01 +0530

    hue slider make smooth
    Signed-off-by: Jeeva Kandasamy <jkandasa@gmail.com>

--- src/Components/Color/HueSlider/HueSlider.jsx
+++ src/Components/Color/HueSlider/HueSlider.jsx
@@ -1,24 +1,32 @@
 import { Badge, Split, SplitItem } from "@patternfly/react-core"
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { HuePicker } from "react-color"
 import "./HueSlider.scss"
 
 const HueSlider = ({ hue = 230, onChange = () => {}, className = "" }) => {
-  const color = { h: hue, s: 0, l: 0.1 }
+  const [hueColor, setHueColor] = useState(0)
+
+  useEffect(() => {
+    setHueColor(hue)
+  }, [])
+
   return (
     <Split className="mc-hue-slider">
       <SplitItem isFilled className={`slider ${className}`}>
         <HuePicker
           className="slider"
           key="hue-picker"
-          color={color}
+          color={{ h: hueColor, s: 0, l: 0.1 }}
+          onChange={(newColor) => {
+            setHueColor(newColor.hsl.h.toFixed(0))
+          }}
           onChangeComplete={(newColor) => {
-            onChange(newColor.hsl.h)
+            onChange(newColor.hsl.h.toFixed(0))
           }}
         />
       </SplitItem>
       <SplitItem className="badge">
-        <Badge key={1}>{color.h}</Badge>
+        <Badge key={1}>{hue}</Badge>
       </SplitItem>
     </Split>
   )
