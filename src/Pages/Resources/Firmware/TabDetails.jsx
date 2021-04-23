a3935fe946eff51800c523a10a08996fe6665572
Jeeva Kandasamy <jkandasa@gmail.com>
2021-04-23 17:40:36 +0530

    list nodes on firmware page
    Signed-off-by: Jeeva Kandasamy <jkandasa@gmail.com>

--- src/Pages/Resources/Firmware/TabDetails.jsx
+++ src/Pages/Resources/Firmware/TabDetails.jsx
@@ -1,8 +1,11 @@
 import React from "react"
 import TabDetailsBase from "../../../Components/BasePage/TabDetailsBase"
+import { RouteLink } from "../../../Components/Buttons/Buttons"
 import { KeyValueMap, Labels } from "../../../Components/DataDisplay/Label"
+import { getStatus } from "../../../Components/Icons/Icons"
 import { LastSeen } from "../../../Components/Time/Time"
 import { api } from "../../../Service/Api"
+import { routeMap as rMap } from "../../../Service/Routes"
 
 const tabDetails = ({ resourceId, history }) => {
   return (
@@ -10,6 +13,11 @@ const tabDetails = ({ resourceId, history }) => {
       resourceId={resourceId}
       history={history}
       apiGetRecord={api.firmware.get}
+      apiListTablesRecord={api.node.list}
+      tableTitle="Assigned Nodes"
+      getTableFilterFunc={getTableFilterFuncImpl}
+      tableColumns={tableColumns}
+      getTableRowsFunc={getTableRowsFuncImpl}
       getDetailsFunc={getDetailsFuncImpl}
     />
   )
@@ -34,4 +42,41 @@ const getDetailsFuncImpl = (data) => {
     "list-1": fieldsList1,
     "list-2": fieldsList2,
   }
+}
+
+const tableColumns = [
+  { title: "Node ID", fieldKey: "nodeId", sortable: true },
+  { title: "Name", fieldKey: "name", sortable: true },
+  { title: "Version", fieldKey: "labels.version", sortable: true },
+  { title: "Library Version", fieldKey: "labels.library_version", sortable: true },
+  { title: "Status", fieldKey: "state.status", sortable: true },
+  { title: "Last Seen", fieldKey: "lastSeen", sortable: true },
+]
+
+const getTableRowsFuncImpl = (rawData, _index, history) => {
+  return [
+    {
+      title: (
+        <RouteLink
+          history={history}
+          path={rMap.resources.node.detail}
+          id={rawData.id}
+          text={rawData.nodeId}
+        />
+      ),
+    },
+    {
+      title: (
+        <RouteLink history={history} path={rMap.resources.node.detail} id={rawData.id} text={rawData.name} />
+      ),
+    },
+    rawData.labels.version,
+    rawData.labels.library_version,
+    { title: getStatus(rawData.state.status) },
+    { title: <LastSeen date={rawData.lastSeen} /> },
+  ]
+}
+
+const getTableFilterFuncImpl = (data) => {
+  return { "labels.assigned_firmware": data.id }
 }
