05a7925bfb72596c5483d6e211b33bd7ee7bf35e
Jeeva Kandasamy <jkandasa@gmail.com>
2021-05-18 21:26:09 +0530

    encrypted identifier
    Signed-off-by: Jeeva Kandasamy <jkandasa@gmail.com>

--- src/Constants/Common.js
+++ src/Constants/Common.js
@@ -12,6 +12,8 @@ export const DATA_CACHE_TIMEOUT = 0 // in seconds, data cache in list tables. 0 
 
 export const NAV_WIDTH = 290
 
+export const SECRET_ENCRYPTION_PREFIX = "ENCRYPTED;MC;AES256;BASE64;"
+
 export const getWindowWidth = (windowSizeWidth, isNavOpen) => {
   if (windowSizeWidth < 1200) {
     return windowSizeWidth
