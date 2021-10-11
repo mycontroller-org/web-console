43206d0ecc860929aa5ad4a8fb2df44e5f75e2cd
Jeeva Kandasamy <jkandasa@gmail.com>
2021-10-11 11:58:23 +0530

    remove icons pack
    Signed-off-by: Jeeva Kandasamy <jkandasa@gmail.com>

--- src/Components/Widgets/ImagePanel/IconHelper.jsx
+++ src/Components/Widgets/ImagePanel/IconHelper.jsx
@@ -1,53 +1,56 @@
 import React from "react"
-import * as AntDesignIcons from "react-icons/ai"
-import * as BootstrapIcons from "react-icons/bs"
-import * as BoxIcons from "react-icons/bi"
-import * as Devicons from "react-icons/di"
-import * as Feather from "react-icons/fi"
+// import * as AntDesignIcons from "react-icons/ai"
+// import * as BootstrapIcons from "react-icons/bs"
+// import * as BoxIcons from "react-icons/bi"
+// import * as Devicons from "react-icons/di"
+// import * as Feather from "react-icons/fi"
 import * as FlatColorIcons from "react-icons/fc"
-import * as FontAwesome from "react-icons/fa"
-import * as GameIcons from "react-icons/gi"
-import * as GithubOcticonsIcons from "react-icons/go"
-import * as GrommetIcons from "react-icons/gr"
-import * as Heroicons from "react-icons/hi"
-import * as IcoMoonFree from "react-icons/im"
-import * as Ionicons4 from "react-icons/io"
-import * as Ionicons5 from "react-icons/io5"
-import * as MaterialDesignIcons from "react-icons/md"
-import * as RemixIcon from "react-icons/ri"
-import * as SimpleIcons from "react-icons/si"
-import * as Typicons from "react-icons/ti"
-import * as VSCodeIcons from "react-icons/vsc"
+// import * as FontAwesome from "react-icons/fa"
+// import * as GameIcons from "react-icons/gi"
+// import * as GithubOcticonsIcons from "react-icons/go"
+// import * as GrommetIcons from "react-icons/gr"
+// import * as Heroicons from "react-icons/hi"
+// import * as IcoMoonFree from "react-icons/im"
+// import * as Ionicons4 from "react-icons/io"
+// import * as Ionicons5 from "react-icons/io5"
+// import * as MaterialDesignIcons from "react-icons/md"
+// import * as RemixIcon from "react-icons/ri"
+// import * as SimpleIcons from "react-icons/si"
+// import * as Typicons from "react-icons/ti"
+// import * as VSCodeIcons from "react-icons/vsc"
 import * as WeatherIcons from "react-icons/wi"
-import * as CSS_gg from "react-icons/cg"
+// import * as CSS_gg from "react-icons/cg"
 import * as PfIcons from "@patternfly/react-icons"
 import { IconType } from "../../../Constants/Widgets/ImagePanel"
 import { getValue } from "../../../Util/Util"
 
 const icons = {
-  ai: AntDesignIcons,
-  bs: BootstrapIcons,
-  bi: BoxIcons,
-  di: Devicons,
-  fi: Feather,
+  // ai: AntDesignIcons,
+  // bs: BootstrapIcons,
+  // bi: BoxIcons,
+  // di: Devicons,
+  // fi: Feather,
   fc: FlatColorIcons,
-  fa: FontAwesome,
-  gi: GameIcons,
-  go: GithubOcticonsIcons,
-  gr: GrommetIcons,
-  hi: Heroicons,
-  im: IcoMoonFree,
-  io: Ionicons4,
-  io5: Ionicons5,
-  md: MaterialDesignIcons,
-  ri: RemixIcon,
-  si: SimpleIcons,
-  ti: Typicons,
-  vsc: VSCodeIcons,
+  // fa: FontAwesome,
+  // gi: GameIcons,
+  // go: GithubOcticonsIcons,
+  // gr: GrommetIcons,
+  // hi: Heroicons,
+  // im: IcoMoonFree,
+  // io: Ionicons4,
+  // io5: Ionicons5,
+  // md: MaterialDesignIcons,
+  // ri: RemixIcon,
+  // si: SimpleIcons,
+  // ti: Typicons,
+  // vsc: VSCodeIcons,
   wi: WeatherIcons,
-  cg: CSS_gg,
+  // cg: CSS_gg,
   pf: PfIcons,
 }
+
+// To reduce the overall build size and to avoid out of memory in node build
+// commented out most of the icons pack
 
 // output: FcEmptyBattery => ['Fc', 'Empty', 'Battery']
 const iconNameRegex = /[A-Z][a-z]+|[0-9]+/g
