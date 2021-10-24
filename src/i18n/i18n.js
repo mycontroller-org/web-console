904f67662d3b6467ae9b05ece64e73199bc127eb
Jeeva Kandasamy <jkandasa@gmail.com>
2021-10-24 19:47:49 +0530

    include tx config and changes in i18n
    Signed-off-by: Jeeva Kandasamy <jkandasa@gmail.com>

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,41 +1,23 @@
 import i18n from "i18next"
 import { initReactI18next } from "react-i18next"
-
-// the translations
-// (tip move them in a JSON file and import them,
-// or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
-const resources = {
-  en: {
-    translation: {
-      dashboard: "Dashboard",
-      about: "About",
-    },
-  },
-  fr: {
-    translation: {
-      dashboard: "tableau de bord",
-    },
-  },
-  ta: {
-    translation: {
-      dashboard: "டாஷ்போர்டு",
-      about: "என்னை பற்றி",
-      forum: "மன்றம்",
-      settings: "அமைப்புகள்",
-      source_code: "மூல குறியீடு",
-      documentation: "ஆவணங்கள்",
-    },
-  },
-}
+import YAML from "js-yaml"
+import Backend from "i18next-http-backend"
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
+  .use(Backend)
   .init({
-    resources,
-    lng: "ta", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+    ns: "translation",
+    fallbackLng: "en_GB",
+    lng: "en_GB", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
-
+    backend: {
+      loadPath: "/locales/{{lng}}/{{ns}}.yaml",
+      parse: function (data) {
+        return YAML.load(data)
+      },
+    },
     interpolation: {
       escapeValue: false, // react already safes from xss
     },
@@ -46,91 +28,91 @@ export default i18n
 // flags unicode, https://emojipedia.org/flags/
 export const languageList = [
   {
-    lng: "en_gb",
+    lng: "en_GB",
     country_code: "UK",
     title: "English",
     flag: "🇬🇧",
   },
   {
-    lng: "en_us",
+    lng: "en_US",
     country_code: "US",
     title: "English",
     flag: "🇺🇸",
   },
   {
-    lng: "de_de",
+    lng: "de_DE",
     country_code: "DE",
     title: "Deutsch",
     flag: "🇩🇪",
   },
   {
-    lng: "es_es",
+    lng: "es_ES",
     country_code: "ES",
     title: "Español",
     flag: "🇪🇸",
   },
   {
-    lng: "fr_fr",
+    lng: "fr_FR",
     country_code: "FR",
     title: "Français",
     flag: "🇫🇷",
   },
   {
-    lng: "he_il",
+    lng: "he_IL",
     country_code: "IL",
     title: "עִברִית",
     flag: "🇮🇱",
   },
   {
-    lng: "it_it",
+    lng: "it_IT",
     country_code: "IT",
     title: "italiano",
     flag: "🇮🇹",
   },
   {
-    lng: "nl_nl",
+    lng: "nl_NL",
     country_code: "NL",
     title: "Nederlands",
     flag: "🇳🇱",
   },
   {
-    lng: "pl_pl",
+    lng: "pl_PL",
     country_code: "PL",
     title: "język polski",
     flag: "🇵🇱",
   },
   {
-    lng: "pt_pt",
+    lng: "pt_PT",
     country_code: "PT",
     title: "Português",
     flag: "🇵🇹",
   },
   {
-    lng: "ro_ro",
+    lng: "ro_RO",
     country_code: "RO",
     title: "Română",
     flag: "🇷🇴",
   },
   {
-    lng: "ru_ru",
+    lng: "ru_RU",
     country_code: "RU",
     title: "Русский",
     flag: "🇷🇺",
   },
   {
-    lng: "ta_in",
+    lng: "ta_IN",
     country_code: "IN",
     title: "தமிழ்",
     flag: "🇮🇳",
   },
   {
-    lng: "zh_cn",
+    lng: "zh_CN",
     country_code: "CN",
     title: "中文",
     flag: "🇨🇳",
   },
   {
-    lng: "zh_tw",
+    lng: "zh_TW",
     country_code: "TW",
     title: "中華民國國語",
     flag: "🇹🇼",
