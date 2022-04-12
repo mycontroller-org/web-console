2ab4ca0510b60285887dfbb745624cd5b64a3696
Jeeva Kandasamy <jkandasa@gmail.com>
2022-04-12 08:40:44 +0530

    update UtilizationPanel edit file
    Signed-off-by: Jeeva Kandasamy <jkandasa@gmail.com>

--- src/Components/Widgets/UtilizationPanel/Table/Edit.js
+++ src/Components/Widgets/UtilizationPanel/Table/Edit.js
@@ -3,58 +3,6 @@ import { DataType, FieldType } from "../../../../Constants/Form"
 import { getValue } from "../../../../Util/Util"
 import { getResourceItems } from "../Common/edit_resource"
 
-// example data structure
-const data = {
-  type: "", // circle_size_100
-  resource: {
-    isMixedResources: false,
-    resources: [
-      {
-        type: "",
-        quickId: "",
-        displayName: "",
-        nameKey: "",
-        roundDecimal: "",
-        valueKey: "",
-        timestampKey: "",
-        unit: "",
-        table: {
-          useGlobal: true,
-          maximumValue: 100,
-          minimumValue: 0,
-          displayStatusPercentage: true,
-          thresholds: {
-            22: "#0066CC",
-            aa: "#0066CC",
-          },
-        },
-      },
-    ],
-    type: "",
-    filterType: "", // quick_id, detailed_filter
-    quickId: "",
-    displayName: "",
-    nameKey: "",
-    roundDecimal: "",
-    valueKey: "",
-    timestampKey: "",
-    unit: "",
-    filter: {},
-  },
-  table: {
-    maximumValue: 100,
-    minimumValue: 0,
-    displayStatusPercentage: true,
-    hideValueColumn: false,
-    hideStatusColumn: false,
-    hideBorder: false,
-    thresholds: {
-      22: "#0066CC",
-      aa: "#0066CC",
-    },
-  },
-}
-
 // Table items
 export const getTableItems = (rootObject) => {
   // load default values
